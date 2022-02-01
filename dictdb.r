8328d9956c6a8838f269179e61f3c08637911134
rebeclem <rebeclem@gmail.com>
2022-02-01 15:59:12 -0500

    bunch of stuff

--- dictdb.r
+++ dictdb.r
@@ -1,7 +1,40 @@
-# This file is to fix up the dictdb dataset
+# This file is to fix up the dictdb dataset (https://drive.google.com/file/d/0B5NJB7U2TQKNVEZneUhFODZFdmc/view)
 #Rebecca Clement
-# April 2, 2021
+# began April 24,2021
+# last updated Jan 1, 2022
 
+#https://github.com/jonathanylin/16S_database_formatting_DADA2
+# This website shows a way to use the taxonomy files given here.
+
+#Load libraries
+library(stringr)
+library(dplyr)
+setwd("~/Box/termite_microbiome/bacterial_refs")
+fasta<-read.table("DictDb_v3.fasta")
+taxonomy<-read.table("DictDB_V3_modified.taxonomy")
+head(fasta)
+# make a new dataframe with same number of rows and columns as taxonomy file
+combined<-select(taxonomy,V1)
+combined$V1<-NA
+combined$V2<-NA
+
+# This loop goes through taxonomy and taxa files and pastes matching taxa id with a ">" into new dataframe "combined", pastes matching sequence to second column, should take about 1 hour.
+for (i in taxonomy$V1) {
+  for (ii in fasta$V1) {
+    if (i == ii) {
+      combined$V1[which(taxonomy$V1 == i)] = paste(">Bacteria;", taxonomy$V2[which(taxonomy$V1 == i)], sep = "")
+      R = which(fasta$V1 == ii) + 1
+      combined$V2[which(taxonomy$V1 == i)] = paste(fasta$V1[R], sep = "")
+    }
+  }
+}
+# Combine two columns into a fasta-ready format
+combined_fasta <- do.call(rbind, lapply(seq(nrow(combined)), function(i) t(combined[i, ])))
+
+head(combined_fasta)
+write.table(combined_fasta, "DictDB_DADA2_FINAL.fasta", row.names = FALSE, col.names = FALSE, quote = FALSE)
+
+# First stuff I did--I think I got Bryan to help me.
 #Set working directory
 setwd("~/Box/Clement_TermiteGut_16S_0108/refs/")
 #First take a look at other references. 
